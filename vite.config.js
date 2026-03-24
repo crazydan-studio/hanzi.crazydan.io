67c2735687455617ecf9eb3f780bfbea99db6c5d
flytreeleft <flytreeleft@crazydan.org>
2026-03-24 15:20:55 +0800

    调整构建脚本以支持压缩 html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import htmlMinifier from 'vite-plugin-html-minifier';
 
 const srcDir = resolve(__dirname, 'src');
 const thanksHtml = readFileSync(resolve(srcDir, 'thanks.html'), 'utf-8');
@@ -12,8 +13,10 @@ export default defineConfig({
   publicDir: resolve(__dirname, 'public'),
   build: {
     outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
     rollupOptions: {
       input: {
+        'index.html': resolve(srcDir, 'index.html'),
         zi: resolve(srcDir, 'zi/index.html'),
         'zi/commons': resolve(srcDir, 'zi/commons/index.html'),
         pinyin: resolve(srcDir, 'pinyin/index.html')
@@ -22,6 +25,9 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
+    htmlMinifier({
+      minify: true
+    }),
     {
       name: 'inject-html-fragment',
       transformIndexHtml(html) {
