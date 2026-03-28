1f0e704ee83d7523df4ac21c7356e7641e81caac
flytreeleft <flytreeleft@crazydan.org>
2026-03-28 21:56:20 +0800

    采用分页机制展示字形列表页面，以确保页面加载性能

--- src/zi/glyphs/index.js
+++ src/zi/glyphs/index.js
@@ -6,6 +6,9 @@ import { fetchCharGlyphAndStrokes } from '#zi/stroke.js';
 
 import '#index.css';
 
+const PAGE_SIZE = 100;
+let pageData = { current: 0, total: 0 };
+
 fetch('/assets/zi/glyphs.json')
   .then((resp) => {
     if (!resp.ok) {
@@ -14,20 +17,65 @@ fetch('/assets/zi/glyphs.json')
     return resp.json();
   })
   .then((chars) => {
-    render(document.getElementById('template_charGridCard'), {
-      chars: chars.map(convertCharGlyphData)
-    });
-
-    const $nodes = document.querySelectorAll(
-      '.char-glyph-card:not(r-template .char-glyph-card)'
-    );
-    lazyLoadGlyphs($nodes);
+    initPager(pageData, chars);
   })
   .catch((e) => {
     render(document.getElementById('template_charGridNetError'), {
       msg: e.message || '无法获取字形列表，请检查网络或稍后重试。'
     });
   });
+
+function initPager(pageData, data) {
+  let oldCurrent = pageData.current;
+
+  pageData.total = Math.ceil(data.length / PAGE_SIZE);
+
+  const doRender = (page) => {
+    render(document.getElementById('template_charGridPager'), { page });
+
+    document.getElementById('page_gotoInput').onblur = (event) => {
+      try {
+        const current = parseInt(event.target.innerText);
+        updatePageCurrent(current);
+      } catch (e) {}
+
+      event.target.innerText = pageData.current;
+    };
+
+    // -----------------------------------------
+    if (oldCurrent != page.current) {
+      const index = (page.current - 1) * PAGE_SIZE;
+
+      renderGrid(data.slice(index, index + PAGE_SIZE));
+    }
+    oldCurrent = page.current;
+  };
+
+  const updatePageCurrent = (current) => {
+    pageData.current = Math.min(pageData.total, Math.max(1, current));
+
+    doRender(pageData);
+  };
+  updatePageCurrent(1);
+
+  document.getElementById('page_prevBtn').onclick = () => {
+    updatePageCurrent(pageData.current - 1);
+  };
+  document.getElementById('page_nextBtn').onclick = () => {
+    updatePageCurrent(pageData.current + 1);
+  };
+}
+
+function renderGrid(data) {
+  render(document.getElementById('template_charGridCard'), {
+    chars: data.map(convertCharGlyphData)
+  });
+
+  const $nodes = document.querySelectorAll(
+    '.char-glyph-card:not(r-template .char-glyph-card)'
+  );
+  lazyLoadGlyphs($nodes);
+}
 
 function lazyLoadGlyphs(targets) {
   const loadingObserver = new IntersectionObserver((entries) => {
